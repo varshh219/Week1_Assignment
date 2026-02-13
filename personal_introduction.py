3a6713bb2037d987213eaf15c61d39fc7aa3d2e6
Varshitha S <varshitha14321@gmail.com>
2026-02-13 22:49:41 +0530

    Initial commit - Personal Introduction Program

---
+++ personal_introduction.py
@@ -0,0 +1,21 @@
+#Personal Introduction Program
+
+name = input("Hello! What's your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+dream = input("What is your dream career? ")
+city = input("Which city are you from? ")
+goal = input("What is one goal you want to achieve this year? ")
+
+
+print("\n====================================")
+print("         PERSONAL INTRO ")
+print("====================================")
+print(f"Hello {name}!✨🎉 ")
+print(f"At {age} years old, you're doing amazing!🫶")
+print(f"It's awesome that you love {hobby}. 🤗")
+print(f"Becoming a {dream} from {city} sounds fantastic! 🌟")
+print(f"Your goal to '{goal}' is inspiring!✨🎉")
+print("\nKeep learning, growing, and shining bright!✨🫶")
+print("The future is yours! ✨")
+print("====================================")
